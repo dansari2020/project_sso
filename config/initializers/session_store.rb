97f3cec3d3bd7825535a7571ca92d6b061eb7883
Daniel Ansari <d0nsari2020@gmail.com>
2021-04-29 20:00:46 +0700

    fix: update config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Leansso::Application.config.session_store :cookie_store, key: '_leansso_session'
+ProjectSSO::Application.config.session_store :cookie_store, key: '_project_sso_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Leansso::Application.config.session_store :active_record_store
+# ProjectSSO::Application.config.session_store :active_record_store
